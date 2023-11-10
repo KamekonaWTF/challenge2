4f1de63c3fe2fb1bde32dc568aea4110daa32d1a
Angel Fernandez <johndoe@example.com>
2023-11-10 16:24:56 +0100

    challenge2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
  // Aquí tu código
+const boton = document.getElementById('agregar');
+const lista = document.getElementById('lista')
+
+ boton.addEventListener('click',function() {
+   let input = prompt("Añadir elemento a la lista: ");
+   let inputX = document.createElement('li');
+   inputX.innerText = input;
+   lista.appendChild(inputX);
+})
